26001cc25fa1a742037e9438b29f516b129809d2
inherited-window-0818 <robt15@hotmail.com>
2019-11-07 05:31:50 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  i = 1
+  array.each_with_index {|str|
+  puts "#{i}. #{str}"
+  i += 1 
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  finarray = []
+  finarray = array.map{|str|
+  "#{str.capitalize}!"
+  }
+  return finarray
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each{|str|
+  if str.length > 4
+    return true
+  end 
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each{|str|
+  if array.include?(str)
+    return str
+  end 
+  }
+  return nil 
 end
